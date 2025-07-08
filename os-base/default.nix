0932517ccd25f938f9dcc5eedc8bc35bb99ffeda
shooter <WhiteBear05@yandex.ru>
2025-07-08 21:52:51 +0400

    added us locale, bridges update

--- os-base/default.nix
+++ os-base/default.nix
@@ -1,12 +1,12 @@
 {
   imports = [
-    ./sddm.nix
     ./bluetooth.nix
     ./boot.nix
     ./i18n.nix
     ./nix.nix
     ./pipewire.nix
     ./pkgs.nix
+    ./sddm.nix
     ./services.nix
     ./tor.nix
     ./users.nix
